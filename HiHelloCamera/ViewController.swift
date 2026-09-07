14930be59ec1052e0f3ac2ffddd2889a4fb9e1c1
YoungH.Kim <jujuroa@gmail.com>
2026-09-08 06:24:40 +0900

    add project

---
+++ HiHelloCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiHelloCamera
+//
+//  Created by YoungH Kim on 9/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
